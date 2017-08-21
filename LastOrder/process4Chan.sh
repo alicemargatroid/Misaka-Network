550831ceb9acf6794e44f7728c7e058ea6253521
Alice <loveoverwhelming@gmail.com>
2017-08-21 00:30:13 -0400

    Quick turn around update

--- LastOrder/process4Chan.sh
+++ LastOrder/process4Chan.sh
@@ -13,32 +13,43 @@ if [ $? -eq 1 ]; then
     exit 1
 fi
 
-# Start main loop, grabbing all boards one by one
-while true
-do
-
-    echo "Loop"
-    sleep 1
-done
-
-# PROTOTYPE, grabbing a single board
 # Create a name to redirect headers to
 header_file=$(mktemp -u)
 
+# Declare an associative array to store Last-Modified data
+declare -A last_modified
+
 # We'll start If-Modified-Since right now, then grep it out of the header_file later
-if_modified_since=$(date +%a,\ %e\ %b\ %Y\ %H:%M:%S\ GMT)
+#if_modified_since=$(date +%a,\ %e\ %b\ %Y\ %H:%M:%S\ GMT)
 
-# Grab all threads one by one
-while IFS=, read thread_url last_modified; do
+# Declare an associative array to store all this shit into
+declare -A post_cache
 
-	thread=$(curl -sL http://a.4cdn.org/b/thread/$thread_url.json)
+# Start main loop
+while true
+do
+  # Grab each board one by one
+  for board in "$@"
+  do
 
-# Get all threads on board from catalog
-done < <(curl -sL -z "$if_modified_since" -D "$header_file" http://a.4cdn.org/b/threads.json | jq -M . |  grep -e "no" -e "last_modified" | tr -d ' ' | cut -d ':' -f2 | paste -d "" - -)
+    # If we have no Last-Modified data, set it to our current time
+    if [[ ! -v last_modified["$board"] ]]; then
 
-# Grep the Last-Modified field out of the catalog grab
-if_modified_since=$(grep "Last-Modified" "$header_file" | cut -d ' ' -f 2-)
-rm "$header_file"
+      # God I hate Date and Is-Modified-Since; couldn't they have coordinated?
+      last_modified["$board"]=$(date +%a,\ %e\ %b\ %Y\ %H:%M:%S\ GMT)
+    fi
 
-echo "ALL DONE"
+    # Grab all threads one by one
+    while IFS=, read thread_url last_modified; do
 
+      thread=$(curl -sL http://a.4cdn.org/b/thread/$thread_url.json)
+
+    # Get all threads on board from catalog
+    done < <(curl -sL -z "$if_modified_since" -D "$header_file" http://a.4cdn.org/b/threads.json | jq -M . |  grep -e "no" -e "last_modified" | tr -d ' ' | cut -d ':' -f2 | paste -d "" - -)
+
+    # Grep the Last-Modified field out of the catalog grab
+    if_modified_since=$(grep "Last-Modified" "$header_file" | cut -d ' ' -f 2-)
+    rm "$header_file"
+
+  done
+done
